5585018b8ad4277d85fbebfe6d30c21a8fc721be
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-01-15 11:20:59 -0500

    aligned TextInput

--- App.js
+++ App.js
@@ -4,10 +4,21 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 export default function App() {
   return (
     <View style={{ padding: 50 }}>
-      <View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         <TextInput
           placeholder="Goals!"
-          style={{ borderColor: "black", borderWidth: 1, padding: 10 }}
+          style={{
+            borderColor: "black",
+            borderWidth: 1,
+            padding: 10,
+            width: "80%",
+          }}
         />
         <Button title="ADD" />
       </View>
